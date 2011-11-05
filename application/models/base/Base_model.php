436c84d68861c4517e29be827271486999981033
Alex Deloy <ap@rawsteel.de>
2011-11-05 15:27:38 +0100

    Struktur II

---
+++ application/models/base/Base_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Base_model extends CI_Model
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    
+    
+}
---
+++ application/models/map/Map_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Map_model extends CI_Model
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    
+    
+}
