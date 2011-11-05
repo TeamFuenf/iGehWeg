a652a91bc18bfb36a4c8dc92e940142812726995
Philipp <Philipp@Philipps-MacBook-Pro-2.local>
2011-11-05 15:39:53 +0100

    Ordner

---
+++ application/controllers/friends/friends.php
@@ -0,0 +1,9 @@
+<?php
+class Friends extends CI_Controller {
+
+	function index()
+	{
+		$this->load->view('friends/friends_mainview');
+	}
+}
+?>
---
+++ application/views/friends/friends_mainview.php
@@ -0,0 +1,15 @@
+<html>
+	<head></head>
+	<body>
+<?php
+	include('top_bar.php');
+?>
+
+		FriendsView!
+
+<?php
+	include('bottom_bar.php');
+?>
+
+	</body>
+</html>
