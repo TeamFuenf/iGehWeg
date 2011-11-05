e1a9f0e01adc6339a3890bb6f51fb3668ff1860e
Philipp <Philipp@Philipps-MacBook-Pro-2.local>
2011-11-05 17:07:42 +0100

    Autoload Database

--- application/controllers/friends/friends.php
+++ application/controllers/friends/friends.php
@@ -3,7 +3,9 @@ class Friends extends CI_Controller {
 
 	function index()
 	{
+		$this->load->view('base/top_bar');
 		$this->load->view('friends/friends_mainview');
+		$this->load->view('base/bottom_bar');
 	}
 }
 ?>
--- application/views/friends/friends_mainview.php
+++ application/views/friends/friends_mainview.php
@@ -1,15 +1,6 @@
 <html>
 	<head></head>
 	<body>
-<?php
-	include('top_bar.php');
-?>
-
 		FriendsView!
-
-<?php
-	include('bottom_bar.php');
-?>
-
 	</body>
 </html>
